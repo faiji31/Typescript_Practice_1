25043d25d65c6f09e1291cfabee72be9e7323014
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-05-05 23:36:08 +0600

    3rd commit

---
+++ src/NonPrimitive.ts
@@ -0,0 +1,17 @@
+// Array, Object,tuple
+
+
+let bazarList:string[] =["oil", "sugar","eggs","milk"]
+
+let mixedList:(string|number)[]=["oil",1,"eggs",2]
+
+mixedList.push("salt")
+
+
+// tuple
+
+let cordinates:[number,number] = [29,49]
+
+
+
+let faijiNameAndId:[string,number,boolean] = ["faiji",2220717,true]
