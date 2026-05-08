21510d3083ce4bed8b9b6266084f7d5b56e88e4c
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-05-08 20:34:23 +0600

    7th commit

---
+++ src/spread.ts
@@ -0,0 +1,12 @@
+const friends =["rhaim","karim"]
+
+const schlfriends=["sara","jerin","tanisha"]
+
+
+const clzfriends =["musta","tashfiq"]
+
+
+
+friends.push(...schlfriends)
+
+console.log(friends)
